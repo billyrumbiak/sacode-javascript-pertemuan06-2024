fc724a70974ecdd103c070006d97116eafbe5533
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2024-06-07 19:53:42 +0900

    file index dan main

---
+++ main.js
@@ -0,0 +1,14 @@
+// string ""
+const str1 = 'BILLY RUMBIAK';
+
+// string ''
+const str2 = 'BILLY RUMBIAK';
+
+// string ``
+const str3 = `${str2}`;
+
+// console
+// function console
+console.log(str1.toUpperCase());
+console.log(str2.toLowerCase());
+console.log(str3);
